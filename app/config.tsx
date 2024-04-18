b1b07dfb148d3e8918f7b6a5edc0dff8d6d5115b
Developers Digest <124798203+developersdigest@users.noreply.github.com>
2024-04-18 16:39:12 -0400

    Update config.tsx
    update to include llama3 model string

--- app/config.tsx
+++ app/config.tsx
@@ -7,7 +7,7 @@
 export const config = {
     useOllamaInference: false,
     useOllamaEmbeddings: false,
-    inferenceModel: 'mixtral-8x7b-32768', // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama2' etc
+    inferenceModel: 'mixtral-8x7b-32768', // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama3' etc
     inferenceAPIKey: process.env.GROQ_API_KEY, // Groq: process.env.GROQ_API_KEY // OpenAI: process.env.OPENAI_API_KEY // Ollama: 'ollama' is the default
     embeddingsModel: 'text-embedding-3-small', // Ollama: 'llama2', 'nomic-embed-text' // OpenAI 'text-embedding-3-small', 'text-embedding-3-large'
     textChunkSize: 1000, // Recommended to decrease for Ollama
